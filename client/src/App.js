a62fd14ca650f65d75c953e55a89c94d41fb1fc0
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-07-13 01:56:33 +1000

    Added layout and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <div className="Container-fluid">
-      <h1 className="text-center">Hello</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="bg-dark text-light p-4">
+      <h6 className="text-center">
+        All rights reserved &copy; Suman Chaudhary
+      </h6>
+    </div>
+  );
+};
