2c80e54e6c9cf7c3bd9896cdefdc9f0d4129e637
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-07-15 00:35:03 +1000

    Added login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,20 +1,17 @@
 import React from "react";
 import { Form, Input } from "antd";
 import { Link } from "react-router-dom";
-const Register = () => {
+
+const Login = () => {
   //From submit
   const submitHandler = (values) => {
     console.log(values);
   };
-
   return (
     <>
       <div className="register-page">
         <Form layout="vertical" onFinish={submitHandler}>
-          <h1>Register Form</h1>
-          <Form.Item label="Name" name="name">
-            <Input />
-          </Form.Item>
+          <h1>Login Form</h1>
           <Form.Item label="Email" name="email">
             <Input type="email" />
           </Form.Item>
@@ -22,9 +19,9 @@ const Register = () => {
             <Input type="password" />
           </Form.Item>
           <div className="d-flex justify-content-between">
-            <Link to="/login">Already Register ? Click Here To Login</Link>
+            <Link to="/register">Not a user ? Click here to register</Link>
             <button className="btn btn-primary" type="submit">
-              Register
+              Login
             </button>
           </div>
         </Form>
@@ -33,4 +30,4 @@ const Register = () => {
   );
 };
 
-export default Register;
+export default Login;
