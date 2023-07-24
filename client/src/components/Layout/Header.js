a9503ec0742897123294b1a3f370a94e329336a9
SumanChaudhary <sumanchaudhary724@gmail.com>
2023-07-24 23:43:04 +1000

    Added logout

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
 export const Header = () => {
+  const [loginUser, setLoginUser] = useState("");
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      setLoginUser(user);
+    }
+  });
   return (
     <>
       {" "}
@@ -15,6 +22,15 @@ export const Header = () => {
             <Nav className="ms-auto">
               <Nav.Link href="/register">User Register</Nav.Link>
               <Nav.Link href="/login">Login</Nav.Link>
+              <Nav>
+                <p>{loginUser && loginUser.name}</p>
+              </Nav>
+              <button
+                className="btn btn-primary"
+                onClick={() => localStorage.removeItem("user")}
+              >
+                Logout
+              </button>
             </Nav>
           </Navbar.Collapse>
         </Container>
